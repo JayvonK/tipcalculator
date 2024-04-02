68de36362c341f52ae150c84eb321aafdcd0f132
JayvonK <jaykhuth@gmail.com>
2024-04-01 21:39:04 -0700

    basic desktop layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,153 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
 
 export default function Home() {
+  const [bill, setBill] = useState<number>(0);
+  const [people, setPeople] = useState<number>(0);
+  const [personTip, setPersonTip] = useState<string>('0.00');
+  const [totalPer, setTotalPer] = useState<string>('0.00');
+  const [error, setError] = useState<string>('');
+
+
+  const handle5 = () => {
+    console.log(bill);
+    if (bill !== undefined && people !== undefined && people !== 0) {
+      let tip = bill * 0.05;
+      let total = bill + tip;
+      setPersonTip(tip.toFixed(2));
+      setTotalPer((total / people).toFixed(2));
+    } else if (people === undefined || people === 0) {
+      setError("Can't be zero");
+    }
+
+  }
+
+  const handle10 = () => {
+    console.log(bill);
+    if (people !== undefined && people !== 0) {
+      let tip = bill * 0.10;
+      let total = bill + tip;
+      setPersonTip(tip.toFixed(2));
+      setTotalPer((total / people).toFixed(2));
+    } else if (people === undefined || people === 0) {
+      setError("Can't be zero");
+    }
+  }
+
+  const handle15 = () => {
+    console.log(bill);
+    if (people !== undefined && people !== 0) {
+      let tip = bill * 0.15;
+      let total = bill + tip;
+      setPersonTip(tip.toFixed(2));
+      setTotalPer((total / people).toFixed(2));
+    } else if (people === undefined || people === 0) {
+      setError("Can't be zero");
+    }
+  }
+
+  const handle25 = () => {
+    console.log(bill);
+    if (people !== undefined && people !== 0) {
+      let tip = bill * 0.25;
+      let total = bill + tip;
+      setPersonTip(tip.toFixed(2));
+      setTotalPer((total / people).toFixed(2));
+    } else if (people === undefined || people === 0) {
+      setError("Can't be zero");
+    }
+  }
+
+  const handle50 = () => {
+    console.log(bill);
+    if (people !== undefined && people !== 0) {
+      let tip = bill * 0.5;
+      let total = bill + tip;
+      setPersonTip(tip.toFixed(2));
+      setTotalPer((total / people).toFixed(2));
+    } else if (people === undefined || people === 0) {
+      setError("Can't be zero");
+    }
+  }
+
+  const handleCustom = () => {
+
+  }
+
+  const handleBillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBill(Number(e.target.value))
+  }
+
+  const handlePeopleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPeople(Number(e.target.value));
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bgCyan pt-36 px-64 min-h-screen">
+
+      <h1 className="text-center spaceMono tracking-[0.4rem] leading-9 text-2xl mb-20">SPLI <br /> TTER</h1>
+
+      <div className="bg-white p-8 rounded-3xl">
+
+        <div className="grid grid-cols-2 gap-4">
+          <div className="pl-4 pr-7">
+
+            <h1 className="spaceMono mb-2 grayTxt mt-3">Bill</h1>
+            <input className="text-black w-full text-2xl spaceMono text-end min-h-12 bgGray mb-6 px-4 rounded-lg" placeholder="0" type="number" onChange={handleBillChange} />
+
+            <h1 className="spaceMono mb-4 grayTxt">Select Tip %</h1>
+
+            <div className="grid grid-rows-2 grid-cols-3 gap-4 text-2xl spaceMono mb-10">
+              <div>
+                <button className="min-h-12 bgDark flex items-center pb-1 justify-center w-full text-white rounded-md" onClick={handle5}>5%</button>
+              </div>
+              <div>
+                <button className="min-h-12 bgDark flex items-center pb-1 justify-center w-full text-white rounded-md" onClick={handle10}>10%</button>
+              </div>
+              <div>
+                <button className="min-h-12 bgDark flex items-center pb-1 justify-center w-full text-white rounded-md" onClick={handle15}>15%</button>
+              </div>
+              <div>
+                <button className="min-h-12 bgDark flex items-center pb-1 justify-center w-full text-white rounded-md" onClick={handle25}>25%</button>
+              </div>
+              <div>
+                <button className="min-h-12 bgDark flex items-center pb-1 justify-center w-full text-white rounded-md" onClick={handle50}>50%</button>
+              </div>
+              <div>
+                <button className="min-h-12 bgDark flex items-center pb-1 justify-center w-full text-white rounded-md">Custom</button>
+              </div>
+            </div>
+            <div>
+
+            </div>
+
+            <div className="flex justify-between">
+              <h1 className="grayTxt spaceMono mb-2">Number of People</h1>
+              <h1 className="text-red-600 spaceMono">{error}</h1>
+            </div>
+
+            <input className="text-black min-h-12 bgGray text-2xl spaceMono text-end w-full mb-4 px-4 rounded-lg" placeholder="0" type="number" onChange={handlePeopleChange} />
+          </div>
+
+          <div className="rounded-xl bgDark p-10">
+            <div className="flex justify-between">
+              <p className="spaceMono text-white">
+                Tip Amount <br />
+                <span className="grayTxt">/ person</span>
+              </p>
+              <h1 className="text-5xl text-end spaceMono">
+                ${personTip}
+              </h1>
+            </div>
+            <h1>Tip Amount: {personTip}</h1>
+            <h1>Total: {totalPer}</h1>
+          </div>
         </div>
+
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
